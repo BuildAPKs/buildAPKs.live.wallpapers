18080712c515d60396fdedcc2266266daf5fa544
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-07-02 02:03:55 -0400

    modified:   gsa.sh

--- gsa.sh
+++ gsa.sh
@@ -12,3 +12,9 @@ git submodule add https://github.com/MorpheusLee/vertex-wallpaper
 git submodule add https://github.com/orac/bubblechamber
 git submodule add https://github.com/PuZZleDucK/Flare-Live-Wallpaper
 git submodule add https://github.com/ssaurel/android-clock-livewallpaper
+curl -OL https://github.com/b3hzad1/android-target-live-wallpaper/tarball/765bdbd80e3eb163ca912342f1b778488ecf90c6
+tar xvf 765bdbd80e3eb163ca912342f1b778488ecf90c6
+rm -f 765bdbd80e3eb163ca912342f1b778488ecf90c6
+curl -OL https://github.com/PuZZleDucK/android-target-live-wallpaper/tarball/4c59a6086942fecffb21b8ed10725d383838a464
+tar xvf 4c59a6086942fecffb21b8ed10725d383838a464
+rm -f 4c59a6086942fecffb21b8ed10725d383838a464
