b5aad64dd49604ae51ff2e1e276beb8937080b64
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-10-24 06:37:28 -0400

    new file:   do.sums.bash 	modified:   ma.bash 	deleted:    make.sums.bash 	deleted:    md5sum.sum 	deleted:    sha1sum.sum 	deleted:    sha256sum.sum 	renamed:    sha512sum.sum -> sha512.sum

--- ma.bash
+++ ma.bash
@@ -1,43 +1,13 @@
-#!/bin/env bash
+#!/bin/env bash 
 # Copyright 2019 (c) all rights reserved 
-# by BuildAPKs https://buildapks.github.io
+# by BuildAPKs https://buildapks.github.io ; See LICENSE
+# File ~/RDR/conf/README.md has more information about this file.
 #####################################################################
 set -Eeuo pipefail
 shopt -s nullglob globstar
-
-_SMATRPERROR_() { # run on script error
-	local RV="$?"
-	echo "$RV" ma.bash
-	printf "\\e[?25h\\n\\e[1;48;5;138mBuildAPKs %s ERROR:  Generated script error %s near or at line number %s by \`%s\`!\\e[0m\\n" "${PWD##*/}" "${1:-UNDEF}" "${2:-LINENO}" "${3:-BASH_COMMAND}"
-	exit 147
-}
-
-_SMATRPEXIT_() { # run on exit
-	printf "\\e[?25h\\e[0m"
-	set +Eeuo pipefail 
-	exit
-}
-
-_SMATRPSIGNAL_() { # run on signal
-	local RV="$?"
-	_WAKEUNLOCK_
-	printf "\\e[?25h\\e[1;7;38;5;0mBuildAPKs %s WARNING:  Signal %s received!\\e[0m\\n" "ma.bash" "$RV"
- 	exit 148 
-}
-
-_SMATRPQUIT_() { # run on quit
-	local RV="$?"
-	_WAKEUNLOCK_
-	printf "\\e[?25h\\e[1;7;38;5;0mBuildAPKs %s WARNING:  Quit signal %s received!\\e[0m\\n" "ma.bash" "$RV"
- 	exit 149 
-}
-
-trap '_SMATRPERROR_ $? $LINENO $BASH_COMMAND' ERR 
-trap _SMATRPEXIT_ EXIT
-trap _SMATRPSIGNAL_ HUP INT TERM 
-trap _SMATRPQUIT_ QUIT 
-
+. "$RDR"/scripts/bash/shlibs/trap.bash 210 211 212
 cd "$JDR"
+# _AT_ username/repository commit
 _AT_ chiralcode/Android-Live-Wallpaper-Template 2bb67a9cca2ddb5f75093e6b285bc9271c71607e
 _AT_ emmaguy/FruityLiveWallpaper b74f15b9d01cf8cef5569377a593fa5373cb0207
 _AT_ givanse/Flowords c4c01cf68531c4b4270926b0536ec20a5407dc9a
@@ -51,5 +21,4 @@ _AT_ orac/bubblechamber 305476d71faf7122258a500353a68fd5fedff81d
 _AT_ PuZZleDucK/android-target-live-wallpaper 4c59a6086942fecffb21b8ed10725d383838a464
 _AT_ PuZZleDucK/Flare-Live-Wallpaper cadecfa9f035e39a96e87adb5a973ca3a91ef4c3 
 _AT_ ssaurel/android-clock-livewallpaper 30d8131e0fe8c5787673b9e6f0fd5abc703749aa
-
 # ma.bash OEF
