4e10665a2f8f9f67d69610602326b2ecfd36452f
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-04-02 22:09:53 -0400

    modified:   ma.bash 	modified:   sha512.sum

--- ma.bash
+++ ma.bash
@@ -8,6 +8,9 @@ shopt -s nullglob globstar
 . "$RDR"/scripts/bash/shlibs/trap.bash 210 211 212
 cd "$JDR"
 # _AT_ username/repository commit
+_AT_ MorpheusLee/vertex-wallpaper a26e413e3aac17b84e547b8ecb89358614111544
+_AT_ PuZZleDucK/android-target-live-wallpaper 4c59a6086942fecffb21b8ed10725d383838a464
+_AT_ PuZZleDucK/Flare-Live-Wallpaper cadecfa9f035e39a96e87adb5a973ca3a91ef4c3 
 _AT_ cameronmurphy/super-mario-live-wallpaper 7ca9dfadfb924ad7441564d242b8ef6a6139d941
 _AT_ chiralcode/Android-Live-Wallpaper-Template 2bb67a9cca2ddb5f75093e6b285bc9271c71607e
 _AT_ emmaguy/FruityLiveWallpaper b74f15b9d01cf8cef5569377a593fa5373cb0207
@@ -15,12 +18,10 @@ _AT_ givanse/Flowords c4c01cf68531c4b4270926b0536ec20a5407dc9a
 _AT_ givanse/Live-Wallpaper 0c3195e8354b1b9287461b8fccfba4fb1a84f56f
 _AT_ gwhiteside/abstract-art da9f047c50af9df02a2233b2a96f43a77e2e4e17
 _AT_ kirill-grouchnikov/android-live-wallpaper e73040e40fb13b46c9a3a30e5d8cfb1e1a3c7d53
+_AT_ mandeeps/TIMLiveWallpaper ce4688b5e0dfc0677cb36317fe93109877ad52b2
 _AT_ matthewmichihara/lanterns-live-wallpaper ccbda7b46aed1716be5322b47326203092e86494
 _AT_ matthewmichihara/spontaneous-combustion cc602b8ab3cf97f3d561f00e6c12e15b50a7b3ff
-_AT_ MorpheusLee/vertex-wallpaper a26e413e3aac17b84e547b8ecb89358614111544
 _AT_ orac/bubblechamber 305476d71faf7122258a500353a68fd5fedff81d
-_AT_ PuZZleDucK/android-target-live-wallpaper 4c59a6086942fecffb21b8ed10725d383838a464
-_AT_ PuZZleDucK/Flare-Live-Wallpaper cadecfa9f035e39a96e87adb5a973ca3a91ef4c3 
 _AT_ shakalaca/BeautyClockLiveWallpaper 8abb9d220c780f5a9d4434de716f71b36714720a
 _AT_ shakalaca/LogcatLiveWallpaper 857fff2d22c96487b517c2a24440d767e16ce2e8
 _AT_ ssaurel/android-clock-livewallpaper 30d8131e0fe8c5787673b9e6f0fd5abc703749aa
