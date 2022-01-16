478c71b6ed2590380252367fe11408bb8d3ced2f
SDRausty <27742457+SDRausty@users.noreply.github.com>
2022-01-16 03:32:24 -0500

    commit 262851250 on 20220116

--- ma.bash
+++ ma.bash
@@ -1,5 +1,5 @@
-#!/bin/env bash 
-# Copyright 2019 (c) all rights reserved 
+#!/bin/env bash
+# Copyright 2019 (c) all rights reserved
 # by BuildAPKs https://buildapks.github.io ; See LICENSE
 # File ~/RDR/conf/README.md has more information about this file.
 #####################################################################
@@ -10,7 +10,7 @@ cd "$JDR"
 # _AT_ username/repository commit
 _AT_ MorpheusLee/vertex-wallpaper a26e413e3aac17b84e547b8ecb89358614111544
 _AT_ Northkastt/android-madrepatria 52d3fb015ceffcc55072babe62d2f8ed80b52ece
-_AT_ PuZZleDucK/Flare-Live-Wallpaper cadecfa9f035e39a96e87adb5a973ca3a91ef4c3 
+_AT_ PuZZleDucK/Flare-Live-Wallpaper cadecfa9f035e39a96e87adb5a973ca3a91ef4c3
 _AT_ PuZZleDucK/android-target-live-wallpaper 4c59a6086942fecffb21b8ed10725d383838a464
 _AT_ SinclairRoss/RadianceLiveWallpaper ce120aa0cb8147f6602faa1f213e2c8d7a8fae4b
 _AT_ TheCollective/android_packages_apps_AOCPWallpapers 5f9952fd7827a572ab7d02b8a7b352904547d74b
@@ -26,6 +26,7 @@ _AT_ mandeeps/TIMLiveWallpaper ce4688b5e0dfc0677cb36317fe93109877ad52b2
 _AT_ matthewmichihara/lanterns-live-wallpaper ccbda7b46aed1716be5322b47326203092e86494
 _AT_ matthewmichihara/spontaneous-combustion cc602b8ab3cf97f3d561f00e6c12e15b50a7b3ff
 _AT_ orac/bubblechamber 305476d71faf7122258a500353a68fd5fedff81d
+_AT_ peejaybee/NyanWallpaper 54fff1569c9deb4f0dce7e2e7c638f6f487c9833
 _AT_ philcolbourn/pc-wallpaper 2e9982cb08ad499eb25dcafdf095fe360c08128a
 _AT_ shakalaca/BeautyClockLiveWallpaper 8abb9d220c780f5a9d4434de716f71b36714720a
 _AT_ shakalaca/LogcatLiveWallpaper 857fff2d22c96487b517c2a24440d767e16ce2e8
